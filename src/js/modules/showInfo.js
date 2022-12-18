7a106bc0ee01da3d6c2a6f6dbf5c458ee7e85195
AlexandrM <jibrilfl@gmail.com>
2022-12-18 15:55:21 +0500

    download files

---
+++ src/js/modules/showInfo.js
@@ -0,0 +1,17 @@
+export default class ShowInfo {
+	constructor(triggersSelector) {
+		this.btns = document.querySelectorAll(triggersSelector);
+	}
+
+	init() {
+		this.btns.forEach(btn => {
+			btn.addEventListener('click', () => {
+				const sibling = btn.closest('.module__info-show').nextElementSibling;
+				sibling.classList.add('animated');
+				sibling.classList.add('fadeInDown');
+				sibling.classList.toggle('msg');
+				sibling.style.marginTop = '20px';
+			});
+		});
+	}
+}
